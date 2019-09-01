fd5e490a35b83ea642b985f48c1b902e636ade06
euglenach <midorimushiiii@gmail.com>
2019-09-01 22:49:03 +0900

    GetKey系anyKey系GetAxis系GetMouseButton系GetButton系

---
+++ UniRx/ButtonInputUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+internal static class ButtonInputUtil{
+    internal enum InputType{
+        GetButton,GetButtonDown, GetButtonUp
+    }
+    private static readonly Dictionary<InputType, Func<string, bool>> inputTable = new Dictionary<InputType, Func<string, bool>>{
+        {InputType.GetButton, Input.GetButton},
+        {InputType.GetButtonDown, Input.GetButtonDown},
+        {InputType.GetButtonUp, Input.GetButtonUp}
+    };
+    
+    internal static IObservable<Unit> CreateSubject(InputType inputType, string buttonName) =>
+        Observable.EveryUpdate()
+                  .Where(_ => inputTable[inputType](buttonName))
+                  .AsUnitObservable();
+    
+}
